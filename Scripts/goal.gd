b28bbf3b1b8856fc146255e2ded3412005df2b7d
ElNogara <well.nogara@hotmail.com>
2023-02-13 11:28:40 -0300

    inserindo bloco quebravel de itens e novo inimigo passáro

--- Scripts/goal.gd
+++ Scripts/goal.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
-func _on_goal_body_entered(body):
+onready var changer = get_parent().get_node("Transition_in")
+
+export var path : String
+
+func _on_goal_body_entered(body: Node) -> void:
 	if body.name == "Player":
 		$confetti.emitting = true
+		changer.change_scene(path)
+		Global.checkpoint_pos = 0
